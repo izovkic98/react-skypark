2ca791a3221c1a725a8c74f62e49450f129bd102
Ivan Zovkić <izovkic@tvz.hr>
2022-08-05 15:41:00 +0100

    navbar fixed

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,15 @@
 import logo from './Skypark_logo.png';
 
+
 const NavigationBar = () => {
 
     return (
-        <nav className="navbar navbark-dark bg-dark">
-            <a href="http://skyparkzagreb.com/" className="navbar-brand ms-1">
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="https://reactjs.org" className="navbar-brand ms-1">
                 <img src={logo} className="App-logo" alt="logo" />
             </a>
 
-            <div className='navbar-nav me-auto'>
+            <div className="navbar-nav me-auto">
                 <li className="nav-item">
                     <a href="" className="nav-link">Admin</a>
                 </li>
@@ -17,16 +18,25 @@ const NavigationBar = () => {
                 </li>
             </div>
 
-            <div className='navbar-nav ms-auto'>
+            <div className="navbar-nav ms-auto">
                 <li className="nav-item">
-                    <a href="" className="nav-link">Sign up</a>
+                    <a href="" className="nav-link">Sign Up</a>
                 </li>
-
                 <li className="nav-item">
-                    <a href="" className="nav-link">Sign in</a>
+                    <a href="" className="nav-link"> Sign In</a>
                 </li>
-
             </div>
+
+            <div className="navbar-nav ms-auto">
+                <li className="nav-item">
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">
+                        Sign Out
+                    </a>
+                </li>
+            </div>
+            
         </nav>
     );
 
